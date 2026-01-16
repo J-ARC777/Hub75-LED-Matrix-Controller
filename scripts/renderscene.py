2fb482cfaa713cc0824aa8474bc3383406baf68e
J-ARC777 <jiarc777@gmail.com>
2026-01-15 23:44:37 -0500

    debugging motioncontroller

--- scripts/renderscene.py
+++ scripts/renderscene.py
@@ -7,7 +7,7 @@ from ledmatrix.motioncontroller import MotionController
 
 
 img_path = "../assets/dvdlogo.png"
-img_src = ImageSource(16,16)
+img_src = ImageSource(24,24)
 display = DisplayController()
 
 renderer = Renderer(display)
@@ -15,7 +15,7 @@ img_src.upload(img_path)
 renderer.compositor.add_layer(img_src)
 
 logo_animator = MotionController(img_src)
-logo_animator.set_velocity(0.5, 0.5)
+logo_animator.set_velocity(5.0, 5.0)
 logo_animator.set_screen_bounds(128,64,"bounce")
 
 renderer.register_updatable(logo_animator)
